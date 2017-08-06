964c0350970753390496e86e51bf7d468929b750
Kite <ixkaito@gmail.com>
2017-08-06 14:09:56 +0900

    Show env

--- assets/_js/app.js
+++ assets/_js/app.js
@@ -1,8 +1,8 @@
 const jQuery = require('jquery');
 const $ = jQuery;
 const env = {
-  'osaka':  require('../../data/20170703_osaka.json'),
-  'halkas': require('../../data/halkas.json'),
+  osaka:  require('../../data/20170703_osaka.json'),
+  halkas: require('../../data/halkas.json'),
 };
 const assets = './assets/images/';
 
@@ -10,6 +10,17 @@ console.log(env.osaka[0]);
 console.log(env.halkas[0]);
 
 $(function() {
+
+const now = 16;
+const current = {
+  pressure: env.osaka[now - 1].localPressure,
+  temperature: env.osaka[now - 1].temperature,
+  windspeed: env.osaka[now - 1].windSpeed,
+}
+
+$('#env .pressure').append(current.pressure + 'hPa');
+$('#env .temperature').append(current.temperature + '℃');
+$('#env .windspeed').append(current.windspeed + 'm/s');
 
 let num = 1;
 let fish;
