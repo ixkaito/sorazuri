1b993ff2b64c6113b2ee895d6bf7b0a3281cef04
Kite <ixkaito@gmail.com>
2017-08-06 13:51:58 +0900

    Add fish

--- assets/_js/app.js
+++ assets/_js/app.js
@@ -1,7 +1,24 @@
+const jQuery = require('jquery');
+const $ = jQuery;
 const env = {
   'osaka':  require('../../data/20170703_osaka.json'),
   'halkas': require('../../data/halkas.json'),
 };
+const assets = './assets/images/';
 
 console.log(env.osaka[0]);
 console.log(env.halkas[0]);
+
+$(function() {
+
+let num = 1;
+let fish;
+
+for (var i = 1; i <= num; i++) {
+
+  fish = assets + 'ray.png';
+  $('#fish').append('<li class="fish-' + i + '"><img src="' + fish + '" /></li>');
+
+}
+
+});
